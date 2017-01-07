c258767988e06a9fb606b164ebb3119744fb4ab7
SantiagoOrdonez <santiagoordonez23@gmail.com>
2017-01-07 18:55:06 -0500

    ...

--- app/src/main/java/com/example/santiagoordonez/hackuoft/dto/HackathonDTO.java
+++ app/src/main/java/com/example/santiagoordonez/hackuoft/dto/HackathonDTO.java
@@ -5,5 +5,23 @@ package com.example.santiagoordonez.hackuoft.dto;
  */
 
 public class HackathonDTO {
+    private String title;
+    private String startDate;
+    private String endDate;
+    private int year;
+    private String city;
+    private String host;
+    private int length;
+    private int size;
+    private boolean travel;
+    private boolean prize;
+    private boolean highSchoolers;
+    private int cost;
+    private String facebookURL;
+    private String twitterURL;
+    private String googlePlusURL;
+    private String notes;
+
+
 
 }
