dc748364df7f7722f09d9dff631e32791738041d
SantiagoOrdonez <santiagoordonez23@gmail.com>
2017-01-07 22:23:13 -0500

    dtp

--- app/src/main/java/com/example/santiagoordonez/hackuoft/ShowHackathons.java
+++ app/src/main/java/com/example/santiagoordonez/hackuoft/ShowHackathons.java
@@ -1,9 +1,21 @@
 package com.example.santiagoordonez.hackuoft;
 
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.santiagoordonez.hackuoft.dto.HackathonDTO;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
 
 public class ShowHackathons extends FragmentActivity {
 
@@ -17,5 +29,58 @@ public class ShowHackathons extends FragmentActivity {
 
         //get the view pager object from the view
         viewPager = (ViewPager) findViewById(R.id.viewPager);
+    }
+
+    public class ParseHackathons extends AsyncTask<String,String,String>{
+
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try{
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+
+                String line = "";
+
+                while ((line = reader.readLine()) != null){
+                    buffer.append(line);
+
+                }
+                return buffer.toString();
+
+            }catch(MalformedURLException e){
+                e.printStackTrace();
+
+            }catch(IOException e){
+                e.printStackTrace();
+            } finally {
+                if (connection != null){
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null){
+                        reader.close();
+                    }
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            //start json parsin.
+        }
     }
 }
