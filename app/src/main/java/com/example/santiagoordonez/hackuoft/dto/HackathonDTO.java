1e17667fa6de7e96a3566b20fed89d77ec0f1369
SantiagoOrdonez <santiagoordonez23@gmail.com>
2017-01-07 19:31:52 -0500

    hackathonDTO

--- app/src/main/java/com/example/santiagoordonez/hackuoft/dto/HackathonDTO.java
+++ app/src/main/java/com/example/santiagoordonez/hackuoft/dto/HackathonDTO.java
@@ -23,5 +23,139 @@ public class HackathonDTO {
     private String notes;
 
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public boolean isTravel() {
+        return travel;
+    }
+
+    public void setTravel(boolean travel) {
+        this.travel = travel;
+    }
+
+    public boolean isPrize() {
+        return prize;
+    }
+
+    public void setPrize(boolean prize) {
+        this.prize = prize;
+    }
+
+    public boolean isHighSchoolers() {
+        return highSchoolers;
+    }
+
+    public void setHighSchoolers(boolean highSchoolers) {
+        this.highSchoolers = highSchoolers;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getFacebookURL() {
+        return facebookURL;
+    }
+
+    public void setFacebookURL(String facebookURL) {
+        this.facebookURL = facebookURL;
+    }
+
+    public String getTwitterURL() {
+        return twitterURL;
+    }
+
+    public void setTwitterURL(String twitterURL) {
+        this.twitterURL = twitterURL;
+    }
+
+    public String getGooglePlusURL() {
+        return googlePlusURL;
+    }
+
+    public void setGooglePlusURL(String googlePlusURL) {
+        this.googlePlusURL = googlePlusURL;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String toString(){
+
+        return"";
+    }
+
+
 
 }
