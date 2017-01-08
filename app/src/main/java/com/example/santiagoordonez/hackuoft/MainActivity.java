23f11e5376e4f6eaab563351644ea681e3ce7a45
Jeanlouis Rebello <jeanlouisrebello@gmail.com>
2017-01-08 04:18:37 -0500

    New Login Screen

--- app/src/main/java/com/example/santiagoordonez/hackuoft/MainActivity.java
+++ app/src/main/java/com/example/santiagoordonez/hackuoft/MainActivity.java
@@ -16,10 +16,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
         goToMenu();
+
         final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
