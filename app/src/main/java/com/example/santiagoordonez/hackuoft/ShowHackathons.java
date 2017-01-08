3525cf733dfa12d0d7ecc0425e78eee37289eacd
SantiagoOrdonez <santiagoordonez23@gmail.com>
2017-01-08 04:01:03 -0500

    hackathons parse working version

--- app/src/main/java/com/example/santiagoordonez/hackuoft/ShowHackathons.java
+++ app/src/main/java/com/example/santiagoordonez/hackuoft/ShowHackathons.java
@@ -8,6 +8,10 @@ import android.os.Bundle;
 
 import com.example.santiagoordonez.hackuoft.dto.HackathonDTO;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,6 +19,11 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class ShowHackathons extends FragmentActivity {
@@ -55,12 +64,64 @@ public class ShowHackathons extends FragmentActivity {
                     buffer.append(line);
 
                 }
+                String finalJSON = buffer.toString();
+//                String months[] = {"January","February","March","April","May","June","July","August",
+//                        "September","October","November","December"};
+//                Date today = Calendar.getInstance().getTime();
+//                for(int i = 0; i < months.length;i++){
+//                    if (months[i].startsWith(today.toString().substring(4,6))){
+//                        String currentMonth = months[i];
+//                        int index = i;
+//                        break;
+//                    }
+//                }
+                JSONObject parentObject = new JSONObject(finalJSON);
+                JSONArray parentArray = parentObject.getJSONArray("January");
+                ArrayList<HackathonDTO> hackathons = new  ArrayList<HackathonDTO>();
+                for (int i = 0; i < parentArray.length();i++){
+                    HackathonDTO hackathonDTO = new HackathonDTO();
+                    JSONObject jsonObject =  parentArray.getJSONObject(i);
+                    String title = jsonObject.getString("title");
+                    hackathonDTO.setTitle(title);
+                    String urlobj = jsonObject.getString("url");
+                    hackathonDTO.setUrl(urlobj);
+                    String startDate = jsonObject.getString("startDate");
+                    hackathonDTO.setStartDate(startDate);
+                    String endDate = jsonObject.getString("endDate");
+                    hackathonDTO.setEndDate(endDate);
+                    String year = jsonObject.getString("year");
+                    hackathonDTO.setYear(year);
+                    String city = jsonObject.getString("city");
+                    hackathonDTO.setCity(city);
+                    String host = jsonObject.getString("host");
+                    hackathonDTO.setHost(host);
+                    String length = jsonObject.getString("length");
+                    hackathonDTO.setLength(length);
+                    String size = jsonObject.getString("size");
+                    hackathonDTO.setSize(size);
+                    String travel = jsonObject.getString("travel");
+                    hackathonDTO.setTravel(travel);
+                    String prize = jsonObject.getString("prize");
+                    hackathonDTO.setPrize(prize);
+                    String highSchoolers = jsonObject.getString("highSchoolers");
+                    hackathonDTO.setHighSchoolers(highSchoolers);
+                    String cost = jsonObject.getString("cost");
+                    hackathonDTO.setCost(cost);
+                    String facebookURL = jsonObject.getString("facebookURL");
+                    hackathonDTO.setFacebookURL(facebookURL);
+                    String twitterURL = jsonObject.getString("twitterURL");
+                    hackathonDTO.setTwitterURL(twitterURL);
+                    String notes = jsonObject.getString("notes");
+                    hackathonDTO.setNotes(notes);
+                    hackathons.add(hackathonDTO);
+                }
                 return buffer.toString();
-
             }catch(MalformedURLException e){
                 e.printStackTrace();
 
             }catch(IOException e){
+                e.printStackTrace();
+            }catch(JSONException e){
                 e.printStackTrace();
             } finally {
                 if (connection != null){
